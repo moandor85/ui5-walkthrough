3358c0dfae4496c7ac0bb8d6dd5d73f9b370a112
Torsten Wittig <moandor85@gmail.com>
2020-05-16 14:20:15 +0200

    Dialogs and Fragments

--- controller/HelloPanel.controller.js
+++ controller/HelloPanel.controller.js
@@ -1,9 +1,12 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+  "sap/m/MessageToast",
+  "sap/ui/core/Fragment"
+], function (Controller, MessageToast, Fragment) {
   "use strict";
+
   return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+
     onShowHello : function () {
       // read msg from i18n model
       var oBundle = this.getView().getModel("i18n").getResourceBundle();
@@ -11,6 +14,27 @@ sap.ui.define([
       var sMsg = oBundle.getText("helloMsg", [sRecipient]);
       //show Message
       MessageToast.show(sMsg);
+    },
+    onOpenDialog : function () {
+      var oView = this.getView();
+
+      //create Dialog lazily
+      if(!this.byId("helloDialog")) {
+        //load asynchronous XML Fragment
+        Fragment.load({
+          id: oView.getId(),
+          name: "sap.ui.demo.walkthrough.view.HelloDialog"
+        }).then(function (oDialog) {
+          //connect Dialog to root view of this component (models, lifecycle)
+          oView.addDependent(oDialog);
+          oDialog.open();
+        });
+
+      } else {
+        this.byId("helloDialog").open();
+      }
+
     }
+    
   });
 });
